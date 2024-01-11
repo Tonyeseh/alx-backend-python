e3369956b203f1717f9a5a48a8756bdb895197fb
Tonyeseh <esehtony123@gmail.com>
2024-01-11 12:35:23 +0100

    task 0 - 6 0x00

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""0-add"""
+
+def add(a: float, b: float) -> float:
+    """adds two numbers a and b"""
+    return a + b
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]):
+    """takes a list mxd_lst of integers and floats and 
+    returns their sum as a float."""
+    return sum(mxd_lst)
